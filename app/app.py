ea6369c20d42b653a205ef58ca2df11e906199c6
bhp <bhp@bhps-Mac-mini.tampabay.rr.com>
2023-06-23 21:33:52 -0400

    add flask-login and auth_bp blueprint

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
+from sqlalchemy import select
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_wtf import CSRFProtect
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+csrf = CSRFProtect()
+login_manager = LoginManager()
 
 
 def create_app():
@@ -13,9 +18,22 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    csrf.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth_bp.login'
+    login_manager.login_message_category = 'info'
+
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        id = int(user_id)
+        return db.session.get(User, id)
 
     from .views_bp import views_bp
     app.register_blueprint(views_bp)
+
+    from .auth_bp import auth_bp
+    app.register_blueprint(auth_bp)
 
     # error handling
     @app.errorhandler(404)
--- app/models.py
+++ app/models.py
@@ -1,15 +1,43 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 from typing import Optional
+from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 from .app import db
 
 
-class Template(db.Model):
-
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(32), index=True)
-    type: Mapped[Optional[str]] = mapped_column(String(32))
+    username: Mapped[str] = mapped_column(String(32), unique=True)
+    pwdhash: Mapped[str]
 
+    entries: Mapped[list['Entry']] = relationship(
+        cascade='all, delete-orphan', back_populates='user'
+    )
+
+    def __init__(self, username, password):
+        self.username = username
+        self.pwdhash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pwdhash, password)
+    
     def __repr__(self):
-        return f'Template({self.id}, "{self.name}")'
+        return f'User({self.id}, "{self.name}")'
+
+
+class Entry(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    timestamp: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow, index=True
+    )
+    content: Mapped[Text] = mapped_column(Text)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('user.id'), index=True
+    )
+
+    user: Mapped['User'] = relationship(
+        back_populates='entries'
+    )
